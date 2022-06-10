fa19f1fa94a85510514630ea5df64c636affa2f9
S-Spiegl <steven.spiegl@gmail.com>
2022-06-10 13:14:16 +0100

    simplest pass

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,2 +1,7 @@
-describe #fizzbuzz do
+require './lib/fizzbuzz'
+
+describe '#fizzbuzz' do
+  it 'should return Fizz if a number is divisible by 3' do
+    expect(fizzbuzz(3)).to eq('Fizz')
+  end
 end
