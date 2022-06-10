5d11f40ea8173b454c7ff57150b4844ea888b552
S-Spiegl <steven.spiegl@gmail.com>
2022-06-10 13:20:40 +0100

    passing tests, no allowance for non fizzbuzz numbers

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,14 @@ require './lib/fizzbuzz'
 
 describe '#fizzbuzz' do
   it 'should return Fizz if a number is divisible by 3' do
-    expect(fizzbuzz(3)).to eq('Fizz')
+    expect(fizzbuzz(6)).to eq('Fizz')
+  end
+
+  it 'should return Buzz if a number is divisible by 5' do
+    expect(fizzbuzz(10)).to eq('Buzz')
+  end
+
+  it 'should return FizzBuzz if a number is divisible by both 3 and 5' do
+    expect(fizzbuzz(15)). to eq('FizzBuzz')
   end
 end
